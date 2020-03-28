36a44b29e0352f2880fd843b3fd807e86c637f2e
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-03-28 15:19:16 +0700

    add multiple handlers example

---
+++ examples/multiple_handlers.js
@@ -0,0 +1,12 @@
+const Server = require('..');
+const server = new Server();
+
+server.get('/',
+(req, res) => {
+  res.status(404);
+},
+(req, res) => {
+  res.end('not found but found :v');
+});
+
+server.listen(8080);
