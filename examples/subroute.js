921b18be976f93c054f980d4e89a9d1b0208dfc5
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-04-17 18:53:39 +0700

    sub-routing example

---
+++ examples/subroute.js
@@ -0,0 +1,21 @@
+const Server = require('..');
+const server = new Server();
+
+server.get('/', (req, res) => {
+  res.send('hello');
+});
+
+// sub-routing for /users/*
+const usersRoute = new Server.Router();
+
+usersRoute.get('/', (req, res) => {
+  res.send('users');
+});
+
+usersRoute.get('/:id', (req, res) => {
+  res.json(req.params);
+});
+
+server.use('/users', usersRoute);
+
+server.listen(8080);
